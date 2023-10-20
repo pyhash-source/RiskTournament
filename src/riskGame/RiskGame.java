7935dd82dcf4c40f8e041482cdf543547521bc74
Jean Cardona <j.hop.mtp@gmail.com>
2023-10-21 00:22:08 +0200

    refactor(main menu): extracted code to method
    Extracted code to method to increase redability.
    Commented broken parts in order to be able to test my code.
    Logic will have to be determined between members of the group.
    Organised imports.

--- src/riskGame/RiskGame.java
+++ src/riskGame/RiskGame.java
@@ -5,21 +5,11 @@
  */
 package riskGame;
 
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 /**
  * Classe qui lance une partie.
- * Point d'entr�e du jeu.
+ * Point d'entree du jeu.
  */
 import riskGame.controler.AbstractControler;
 import riskGame.model.AbstractModel;
@@ -31,27 +21,8 @@ public class RiskGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	//Cet endroit est le point d'entree du programme, on va commencer par faire un Menu pour le lancer de partie    	
-    	String[] optionsToChoose = {"Lancer une partie", "Autres options ?..."};
-        String niveauChoisi = (String) JOptionPane.showInputDialog(
-                null,
-                "Que voulez vous faire ? ",
-                "Risk e-sport [MENU]",
-                JOptionPane.PLAIN_MESSAGE,
-                null,
-                optionsToChoose,
-                optionsToChoose[0]);
-        if(niveauChoisi == null)
-        {
-        	niveauChoisi ="noAction";//permet au bouton cancel de fonctionnner
-        	//le bouton cancel affecte null et casse le switch. donc on le rename en une valeur valide
-        } else if(niveauChoisi == "Lancer une partie") {
-        	//LOGIQUE DE LANCEMENT DE PARTIE ICI
-        	System.out.println("Coucou");
-        }
-    }
-    	
-    	/*
+    	mainMenuGUI();
+        /*
         AbstractModel model = null; //à instancier
         AbstractControler controler = null; //à instancier
         Vue v = new Vue(model,controler);
@@ -60,7 +31,28 @@ public class RiskGame {
         while(!model.partieTerminer()){
             controler.calculerStepSuivant();
         }
-        */
+         */
+    }
+
+	private static void mainMenuGUI() {
+		String[] optionsToChoose = {"Lancer une partie", "Autres options ?..."};
+        String choice = (String) JOptionPane.showInputDialog(
+                null,
+                "Que voulez vous faire ? ",
+                "Risk e-sport [MENU]",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                optionsToChoose,
+                optionsToChoose[0]);
+        if(choice == null)
+        {
+        	choice ="noAction";//permet au bouton cancel de fonctionnner
+        } else if(choice == "Lancer une partie") {
+        	//LOGIQUE DE LANCEMENT DE PARTIE ICI
+        	System.out.println("Coucou");
+        }
+	}
+    	
     }
     
 
