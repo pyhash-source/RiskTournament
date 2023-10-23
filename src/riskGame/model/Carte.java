a4dfa57c6da2bcfc84b58874f6ef852a389969f6
Yasmine <yasmine.chakar.et@gmail.com>
2023-10-23 11:04:13 +0200

    feat(classes:Continent,Carte,TypeRegiments)

---
+++ src/riskGame/model/Carte.java
@@ -0,0 +1,19 @@
+package riskGame.model;
+/**
+ * @author: Yasmine
+ */
+public class Carte {
+	
+	//attributs
+	private Territoire territoire;
+	private String typeRegiment;
+	//constructeur
+	public Carte(Territoire territoire, String typeRegiment) {
+		super();
+		this.territoire = territoire;
+		this.typeRegiment = typeRegiment;
+	}
+	
+	
+
+}
