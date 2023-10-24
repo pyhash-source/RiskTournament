6480b4d206a7350534824bdd0cc512679fe3169f
Fitia <rakotondramarofitia@gmail.com>
2023-10-24 15:15:38 +0200

    feat(debutPartie) implements function to determine first player

--- src/riskGame/model/Manche.java
+++ src/riskGame/model/Manche.java
@@ -1,10 +1,15 @@
 package riskGame.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+
 
 /**
- * @author Jean
+ * @author Jean, Fitia
  **/
 
 public class Manche {
@@ -56,6 +61,51 @@ public class Manche {
 
 	public void ajouterJoueur(Joueur joueur) {
 		this.joueursManche.add(joueur);
+	}
+	
+	// Methods
+	
+	/*
+	 * Cette fonction permet de déterminer le premier joueur d'un jeu en utilisant un lancer de dé pour chaque joueur
+	 */
+	
+	public Joueur determinerPremierJoueur() {
+		
+		Random random = new Random();
+		HashMap <Joueur, Integer> resultatLancementDe = new HashMap<>();
+		
+		// Générer des nombres aléatoires pour chaque joueur 
+		for (Joueur joueur : this.joueursManche) {
+		    int randomNumber = random.nextInt(6) + 1;
+		    resultatLancementDe.put(joueur, randomNumber);
+		}
+				
+		// Rechercher le premier joueur en le recherchant dans mon HashMap resultatLancementDe
+        Joueur premierJoueur = null;
+        int maxNumber = 0;
+        
+        Iterator it;
+        it = resultatLancementDe.keySet().iterator();
+        StringBuilder message = new StringBuilder("Résultats du lancement de dé : \n");
+		        
+        while(it.hasNext()) {
+        	Joueur joueurActuel = (Joueur) it.next(); 
+        	Integer number = resultatLancementDe.get(joueurActuel);
+        	
+        	//Afficher le nom du joueur et son score
+        	message.append(joueurActuel.getPrenomJoueur()).append(" : ").append(number).append("\n");
+        	if (number>maxNumber) {
+        		maxNumber=number;
+        		//Déterminer premier joueur
+        		premierJoueur = joueurActuel;
+        	}
+        }
+		        
+        message.append("\nLe premier joueur à jouer est ").append(premierJoueur.getPrenomJoueur());
+       
+        // Affichage popup
+        JOptionPane.showMessageDialog(null, message.toString());
+		return premierJoueur;
 	}
 	
 }
